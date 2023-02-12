6257807855949f38186ed8cb1cb9f92eef63237f
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-02-12 13:57:37 +0530

    Update Info.js

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,15 +1,14 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Info = () => {
-    const navigate = useHistory()
     return (
         <div id="info-page">
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
-            <button id="info-to-blog" onClick={() => navigate.push('/blog')}> Go to Blog</button>
-            <button id="info-to-home" onClick={() => navigate.push('/')}> Go to Home</button>
+            <button id="info-to-blog"><Link to="/blog"> Go to Blog</Link></button>
+            <button id="info-to-home"><Link to="/">Go to Home</Link></button>
         </div>
     )
 }
