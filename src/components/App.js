5b9c2692be7cdb44b3983a1de4b764ed2efe8b4c
Souvick Chakraborty <114563269+githubsouvick7@users.noreply.github.com>
2023-02-12 13:56:20 +0530

    Update App.js
    last

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../styles/App.css';
+// import { BrowserRouter as Router, Route,Link,Switch } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
 import Info from './Info';
 import Blog from './Blog';
 import Home from './Home';
 const App = () => {
+
   return (
     <div id="main">
       <Router>
@@ -12,7 +14,7 @@ const App = () => {
           <Route path="/" exact>
             <Home />
           </Route>
-          <Route path='/Blog'>
+          <Route path='/blog'>
             <Blog />
           </Route>
           <Route path='/Info'>
@@ -23,5 +25,6 @@ const App = () => {
     </div>
   )
 }
+
 
 export default App;
